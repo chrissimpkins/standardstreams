00632c41a0f405bf6b88fca0fd814cf04fde03e4
Chris Simpkins <git.simpkins@gmail.com>
2016-03-12 23:50:51 -0500

    new std out, std err and std in functions

--- lib/standardstreams/library.py
+++ lib/standardstreams/library.py
@@ -1,5 +1,80 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+
+# ///////////////////////////////////////////
+#
+# Standard Output Functions
+#
+# ///////////////////////////////////////////
 
 
+def stdout(msg, statuscode=None):
+    sys.stdout.write(msg + "\n")
+    if statuscode is not None:
+        assert isinstance(statuscode, object)
+        sys.exit(statuscode)
+
+
+def stdout_xnl(msg, statuscode=None):
+    sys.stdout.write(msg)
+    if statuscode is not None:
+        assert isinstance(statuscode, object)
+        sys.exit(statuscode)
+
+
+def stdout_iter(iterable, statuscode=None):
+    for msg in iterable:
+        sys.stdout.write(msg + "\n")
+    if statuscode is not None:
+        assert isinstance(statuscode, object)
+        sys.exit(statuscode)
+
+
+# ///////////////////////////////////////////
+#
+# Standard Error Functions
+#
+# ///////////////////////////////////////////
+
+
+def stderr(msg, statuscode=None):
+    sys.stderr.write(msg + "\n")
+    if statuscode is not None:
+        assert isinstance(statuscode, object)
+        sys.exit(statuscode)
+
+
+def stderr_xnl(msg, statuscode=None):
+    sys.stderr.write(msg)
+    if statuscode is not None:
+        assert isinstance(statuscode, object)
+        sys.exit(statuscode)
+
+
+def stderr_iter(iterable, statuscode=None):
+    for msg in iterable:
+        sys.stderr.write(msg + "\n")
+    if statuscode is not None:
+        assert isinstance(statuscode, object)
+        sys.exit(statuscode)
+
+
+# ///////////////////////////////////////////
+#
+# Standard Input Functions
+#
+# ///////////////////////////////////////////
+
+def stdin():
+    return sys.stdin.read()
+
+
+def stdin_by_line():
+    for line in sys.stdin.readlines():
+        yield line
+
+
+def prompt(msg):
+    pass
