cc15cb9799ad41f9baa0bb5f971e4c86c8fec5ff
Chris Simpkins <git.simpkins@gmail.com>
2016-03-13 10:01:31 -0400

    new docstrings for functions

--- lib/standardstreams/library.py
+++ lib/standardstreams/library.py
@@ -11,6 +11,12 @@ import sys
 
 
 def stdout(msg, statuscode=None):
+    """Writes msg to the standard output stream with option to exit program with the exit status code
+    statuscode.  If statuscode is not defined, sys.exit() is not called by the function.
+
+    :param msg: The data to print in the standard output stream
+    :param statuscode: (optional) an integer that represents the exit status code."""
+
     sys.stdout.write(msg + "\n")
     if statuscode is not None:
         assert isinstance(statuscode, object)
@@ -40,6 +46,12 @@ def stdout(msg, statuscode=None):
 
 
 def stderr(msg, statuscode=None):
+    """Writes msg to the standard error stream with option to exit program with the exit status code
+    statuscode.  If statuscode is not defined, sys.exit() is not called by the function.
+
+    :param msg: The data to print in the standard error stream
+    :param statuscode: (optional) an integer that represents the exit status code."""
+
     sys.stderr.write(msg + "\n")
     if statuscode is not None:
         assert isinstance(statuscode, object)
@@ -68,10 +80,19 @@ def stderr(msg, statuscode=None):
 # ///////////////////////////////////////////
 
 def stdin():
+    """Reads and returns all data in the standard input stream.  The function blocks until all data is read.
+
+    :returns: Standard input stream data
+    """
+
     return sys.stdin.read()
 
 
 def stdin_lines():
+    """A generator function that reads and returns all data in the standard input stream by each newline value.
+
+    :returns: Standard input stream data by newline
+    """
     for line in sys.stdin.readlines():
         yield line
 
