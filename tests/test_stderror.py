adb4ba61012a6fef3d60091fbc95855e6b9d28a1
Chris Simpkins <git.simpkins@gmail.com>
2016-03-14 17:12:38 -0400

    added stderr function tests, new stdout function test

--- tests/test_stderror.py
+++ tests/test_stderror.py
@@ -4,91 +4,86 @@
 import sys
 import pytest
 
-from standardstreams import stdout
+from standardstreams import stderr
 
 
-def test_stdout_capsysfail(capsys):    # confirm that capsys capturing standard out appropriately
-    stdout("bogus text for a test")
+def test_stderr_capsysfail(capsys):    # confirm that capsys capturing standard out appropriately
+    stderr("bogus text for a test")
     out, err = capsys.readouterr()
-    assert out != "something else"
-    assert out == "bogus text for a test\n"
+    assert err != "something else"
+    assert err == "bogus text for a test\n"
 
 
 # ///////////////////////////////////////////
 #
-# stdout() function tests
+# stderr() function tests
 #
 # ///////////////////////////////////////////
 
-
-def test_stdout_ascii(capsys):
-    stdout("This is a test message")
+def test_stderr_ascii(capsys):
+    stderr("This is a test message")
     out, err = capsys.readouterr()
-    assert out == "This is a test message\n"
+    assert err == "This is a test message\n"
 
 
-def test_stdout_unicode(capsys):
+def test_stderr_unicode(capsys):
     if sys.version_info[0] == 2:
         msg = u"カイダーディー"
     else:
         msg = "カイダーディー"
-    stdout(msg)
+    stderr(msg)
     out, err = capsys.readouterr()
-    assert out == msg + "\n"
+    assert err == msg + "\n"
 
 
-def test_stdout_ascii_reports_correct_statuscode_one(capsys):
+def test_stderr_ascii_reports_correct_statuscode_one(capsys):
     with pytest.raises(SystemExit):
         try:
-            stdout("This is a test message", statuscode=1)
+            stderr("This is a test message", statuscode=1)
             out, err = capsys.readouterr()
-            assert out == "This is a test message\n"
+            assert err == "This is a test message\n"
         except SystemExit as exit_code:
             assert exit_code.__str__() == '1'  # tests the exit status code in the SystemExit exception
             raise exit_code
 
 
-def test_stdout_ascii_reports_correct_statuscode_zero(capsys):
+def test_stderr_ascii_reports_correct_statuscode_zero(capsys):
     with pytest.raises(SystemExit):
         try:
-            stdout("This is a test message", statuscode=0)
+            stderr("This is a test message", statuscode=0)
             out, err = capsys.readouterr()
-            assert out == "This is a test message\n"
+            assert err == "This is a test message\n"
         except SystemExit as exit_code:
             assert exit_code.__str__() == '0'  # tests the exit status code in the SystemExit exception
             raise exit_code
 
 
-def test_stdout_ascii_reports_correct_statuscode_minusone(capsys):
+def test_stderr_ascii_reports_correct_statuscode_minusone(capsys):
     with pytest.raises(SystemExit):
         try:
-            stdout("This is a test message", statuscode=-1)
+            stderr("This is a test message", statuscode=-1)
             out, err = capsys.readouterr()
-            assert out == "This is a test message\n"
+            assert err == "This is a test message\n"
         except SystemExit as exit_code:
             assert exit_code.__str__() == '-1'  # tests the exit status code in the SystemExit exception
             raise exit_code
 
 
-def test_stdout_unicode_reports_correct_statuscode(capsys):
+def test_stderr_unicode_reports_correct_statuscode(capsys):
     if sys.version_info[0] == 2:
         msg = u"カイダーディー"
     else:
         msg = "カイダーディー"
     with pytest.raises(SystemExit):
         try:
-            stdout(msg, statuscode=1)
+            stderr(msg, statuscode=1)
             out, err = capsys.readouterr()
-            assert out == msg + "\n"
+            assert err == msg + "\n"
         except SystemExit as exit_code:
             assert exit_code.__str__() == '1'  # tests the exit status code in the SystemExit exception
             raise exit_code
 
 
-def test_stdout_bad_object_as_statuscode():
+def test_stderr_bad_object_as_statuscode():
     with pytest.raises(NameError):
-        stdout("This is a test message", statuscode=bogusobj)
-
-
-
-
+        stderr("This is a test message", statuscode=bogusobj)
