1f1ddbff312831f2aa967f916a416d5d5acaae7e
Chris Simpkins <git.simpkins@gmail.com>
2016-03-16 15:06:50 -0400

    support for begin and end parameters in stdout function, new tests

--- lib/standardstreams/library.py
+++ lib/standardstreams/library.py
@@ -3,6 +3,15 @@
 
 import sys
 
+# Define basestring in Python version independent manner for string tests
+try:
+  basestring
+except NameError:
+  basestring = str
+
+# TODO: stdout_json: std output formatted as json
+# TODO: format the width in characters
+
 # ///////////////////////////////////////////
 #
 # Standard Output Functions
@@ -10,26 +19,41 @@ import sys
 # ///////////////////////////////////////////
 
 
-def stdout(msg, statuscode=None):
-    """Writes msg to the standard output stream with option to exit program with the exit status code
-    statuscode.  If statuscode is not defined, sys.exit() is not called by the function.
+def stdout(msg, begin="", end="\n", statuscode=None):
+    """Writes msg string (or __str__ representation of msg object) to the standard output stream with option
+    to exit program if the statuscode function parameter is defined with an exit status code integer.
 
-    :param msg: The data to print in the standard output stream
+    Strings can be prepended and appended to the msg string with function parameters.  By default, a
+    platform-dependent newline character is appended to the string before the standard output stream write.
+    This behavior can be redefined in the `end` function parameter.
+
+    :param msg: The data to print in the standard output stream. Uses string representation if not a string type
+    :param end: (optional) The string to append to the msg string. Default = '\n'
+    :param begin: (optional) The string to prepend to the msg string. Default = empty string
     :param statuscode: (optional) an integer that represents the exit status code."""
 
-    sys.stdout.write(msg + "\n")
+    # Parameter type tests
+    # (Note: basestring redefined to str for Py3 in module head)
+    if not isinstance(msg, basestring):
+        msg = str(msg)  # cast to str type for any object that is not an instance of basestring (Py2) or str (Py3)
+
+    if not isinstance(begin, basestring):
+        raise TypeError("begin keyword parameter must be defined with a string type")
+
+    if not isinstance(end, basestring):
+        raise TypeError("end keyword parameter must be defined with a string type")
+
     if statuscode is not None:
-        assert isinstance(statuscode, object)
+        if not isinstance(statuscode, int):
+            raise TypeError("statuscode keyword parameter must be defined with an integer exit code value")
+
+    # Write to standard output stream
+    sys.stdout.write(begin + msg + end)
+
+    # Handle request to raise SystemExit at the end of the standard output write
+    if statuscode is not None:
         sys.exit(statuscode)
 
-
-# def stdout_xnl(msg, statuscode=None):
-#     sys.stdout.write(msg)
-#     if statuscode is not None:
-#         assert isinstance(statuscode, object)
-#         sys.exit(statuscode)
-#
-#
 # def stdout_iter(iterable, statuscode=None):
 #     for msg in iterable:
 #         sys.stdout.write(msg + "\n")
@@ -58,13 +82,6 @@ def stderr(msg, statuscode=None):
         sys.exit(statuscode)
 
 
-# def stderr_xnl(msg, statuscode=None):
-#     sys.stderr.write(msg)
-#     if statuscode is not None:
-#         assert isinstance(statuscode, object)
-#         sys.exit(statuscode)
-#
-#
 # def stderr_iter(iterable, statuscode=None):
 #     for msg in iterable:
 #         sys.stderr.write(msg + "\n")
--- tests/test_stdoutput.py
+++ tests/test_stdoutput.py
@@ -7,18 +7,155 @@ import pytest
 from standardstreams import stdout
 
 
-def test_stdout_capsysfail(capsys):    # confirm that capsys capturing standard out appropriately
-    stdout("bogus text for a test")
+# ////////////////////////////////////////////////
+#
+# Setup
+#
+# ////////////////////////////////////////////////
+
+# class definitions for tests
+
+class Bogus(object):
+    def __init__(self):
+        pass
+
+
+class BogusWithStr(object):
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "__str__ success"
+
+
+class BogusWithRepr(object):
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return "__repr__ success"
+
+
+class BogusWithBoth(object):
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "__str__ success"
+
+    def __repr__(self):
+        return "__repr__ success"
+
+
+# ///////////////////////////////////////////////////////
+#
+# pytest capsys capture tests
+#    confirms capture of std output and std error streams
+#
+# ///////////////////////////////////////////////////////
+
+
+def test_pytest_capsys(capsys):
+    print("bogus text for a test")
+    sys.stderr.write("more text for a test")
     out, err = capsys.readouterr()
-    assert out != "something else"
     assert out == "bogus text for a test\n"
+    assert out != "something else"
+    assert err == "more text for a test"
+    assert err != "something else"
 
 
-# ///////////////////////////////////////////
+# ////////////////////////////////////////////////
 #
-# stdout() function tests
+# stdout() parameter testing tests
 #
-# ///////////////////////////////////////////
+# ////////////////////////////////////////////////
+
+def test_stdout_msg_parameter_test_nonstring_casts_ok():
+    stdout(str)  # confirm that string object casts to str without raising exception
+
+
+def test_stdout_msg_parameter_test_nonstring_cast_fails_nonexistent_object():
+    with pytest.raises(NameError):
+        stdout(Fakeola)
+
+
+def test_stdout_msg_parameter_test_object_nostr_norepr(capsys):
+    bogus = Bogus()
+    out, err = capsys.readouterr()
+    stdout(bogus)  # confirm that this does not raise exception
+
+
+def test_stdout_msg_parameter_test_object_str_norepr(capsys):
+    bogus = BogusWithStr()
+    stdout(bogus)
+    out, err = capsys.readouterr()
+    assert out == "__str__ success\n"
+
+
+def test_stdout_msg_parameter_test_object_nostr_repr(capsys):
+    bogus = BogusWithRepr()
+    stdout(bogus)
+    out, err = capsys.readouterr()
+    assert out == "__repr__ success\n"
+
+
+def test_stdout_msg_parameter_test_object_str_repr(capsys):
+    bogus = BogusWithBoth()
+    stdout(bogus)
+    out, err = capsys.readouterr()
+    assert out == "__str__ success\n"
+
+
+def test_stdout_begin_parameter_test_true_ascii(capsys):
+    stdout("This is a test message", begin="[test] ")
+    out, err = capsys.readouterr()
+    assert out == "[test] This is a test message\n"
+
+
+def test_stdout_begin_parameter_test_false_ascii(capsys):
+    with pytest.raises(TypeError):
+        stdout("This is a test message", begin=123)
+        out, err = capsys.readouterr()
+        assert out == "123This is a test message\n"
+
+
+def test_stdout_begin_parameter_test_true_unicode(capsys):
+    if sys.version_info[0] == 2:
+        msg = u"カイダーディー"
+        beg_str = u'ディー '
+    else:
+        msg = "カイダーディー"
+        beg_str = u'ディー '
+    stdout(msg, begin=beg_str)
+    out, err = capsys.readouterr()
+    assert out == beg_str + msg + '\n'
+
+
+def test_stdout_begin_parameter_test_false_unicode(capsys):
+    with pytest.raises(TypeError):
+        if sys.version_info[0] == 2:
+            msg = u"カイダーディー"
+        else:
+            msg = "カイダーディー"
+        beg_str = 123 # define as an integer
+        stdout(msg, begin=beg_str)
+        out, err = capsys.readouterr()
+        assert out == beg_str + msg + '\n'
+
+
+# ////////////////////////////////////////////////
+#
+# stdout() function tests with default parameters
+#
+# ////////////////////////////////////////////////
+
+
+def test_stdout_missing_msg_parameter(capsys):
+    with pytest.raises(TypeError):
+        stdout()
+        out, err = capsys.readouterr()
+        assert out == "\n"
 
 
 def test_stdout_ascii(capsys):
